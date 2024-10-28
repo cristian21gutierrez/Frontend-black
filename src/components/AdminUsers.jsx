17e368081d9278f69c1bcc19ccacf3072b97838f
cristian21gutierrez <cris1997fabiang@gmail.com>
2024-10-28 04:38:29 -0300

    Agrega nuevas funcionalidades

--- src/components/AdminUsers.jsx
+++ src/components/AdminUsers.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import "./styles/AdminUser.css"
+import "./styles/adminUser.css"
 
 const AdminUsers = () => {
     const [users, setUsers] = useState([]);
